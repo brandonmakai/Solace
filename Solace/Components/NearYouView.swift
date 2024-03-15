ea22b11d9f8ed4ce25556eeb266003c550398c96
Brandon Williams <brandonmakaiw@gmail.com>
2024-03-15 17:26:30 -0400

    Added User and Professional Class - Added User and Profesional Struct - Added Mock Data - Added Distance Function

---
+++ Solace/Components/NearYouView.swift
@@ -0,0 +1,18 @@
+//
+//  NearYouView.swift
+//  Solace
+//
+//  Created by Brandon Williams on 3/15/24.
+//
+
+import SwiftUI
+
+struct NearYouView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NearYouView()
+}
---
+++ Solace/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Solace
+//
+//  Created by Brandon Williams on 3/15/24.
+//
+
+import SwiftUI
+
+struct User: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    User()
+}
