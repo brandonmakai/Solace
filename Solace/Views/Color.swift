5eaa3cdb3b031c695a77c50a1c9550268e349dd2
Brandon Williams <brandonmakaiw@gmail.com>
2024-03-15 14:36:33 -0400

    Organized Files - Added Custom Colors

---
+++ Solace/Views/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  Solace
+//
+//  Created by Brandon Williams on 3/15/24.
+//
+
+import SwiftUI
+
+struct Color: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color()
+}
