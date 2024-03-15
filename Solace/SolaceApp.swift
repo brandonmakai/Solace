48fe2924ab8a62b17323ec70a87c4c5a91948498
Brandon Williams <brandonmakaiw@gmail.com>
2024-03-15 13:25:28 -0400

    Initial Commit

---
+++ Solace/SolaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolaceApp.swift
+//  Solace
+//
+//  Created by Brandon Williams on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SolaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
